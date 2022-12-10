28d992343cb2b716e2ffa53d5dec4f8033063d1d
LUTR <v.syreyshchikov@gmail.com>
2022-12-10 21:05:09 +0300

    On branch master  Initial commit [I# Changes to be committed: 	new file:   Bilaia slava (flag).sln 	new file:   Bilaia slava (flag)/Bilaia slava (flag).cpp 	new file:   Bilaia slava (flag)/Bilaia slava (flag).vcxproj 	new file:   Bilaia slava (flag)/Bilaia slava (flag).vcxproj.filters 	new file:   Bilaia slava (flag)/Bilaia slava (flag).vcxproj.user 	new file:   Bilaia slava (flag)/Debug/Bilaia s.ea03ba7a.tlog/Bilaia slava (flag).lastbuildstate 	new file:   Bilaia slava (flag)/Debug/Bilaia s.ea03ba7a.tlog/CL.command.1.tlog 	new file:   Bilaia slava (flag)/Debug/Bilaia s.ea03ba7a.tlog/CL.read.1.tlog 	new file:   Bilaia slava (flag)/Debug/Bilaia s.ea03ba7a.tlog/CL.write.1.tlog 	new file:   Bilaia slava (flag)/Debug/Bilaia s.ea03ba7a.tlog/link.command.1.tlog 	new file:   Bilaia slava (flag)/Debug/Bilaia s.ea03ba7a.tlog/link.read.1.tlog 	new file:   Bilaia slava (flag)/Debug/Bilaia s.ea03ba7a.tlog/link.write.1.tlog 	new file:   Bilaia slava (flag)/Debug/Bilaia slava (flag).exe.recipe 	new file:   Bilaia slava (flag)/Debug/Bilaia slava (flag).ilk 	new file:   Bilaia slava (flag)/Debug/Bilaia slava (flag).log 	new file:   Bilaia slava (flag)/Debug/Bilaia slava (flag).obj 	new file:   Bilaia slava (flag)/Debug/vc142.idb 	new file:   Bilaia slava (flag)/Debug/vc142.pdb

---
+++ Bilaia slava (flag)/Bilaia slava (flag).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 13; ++i)
+    {
+        if (i < 8)
+        {
+            for (int j = 0; j < 6; ++j)
+            {
+                cout << "  *";
+            }
+            cout.width(60);
+            cout << setw(60) << setfill('_'); // выделяем место в 60 символов и заполняем их "_"
+            cout << '\n';
+        }
+    }
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << setw(78) << setfill('_') << '\n'; // setw задает ширину строки поля
+    }
+}
